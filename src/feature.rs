9b0a2892715ae2af0a41fdae01bec69cdc96f031
keiya01 <34934510+keiya01@users.noreply.github.com>
2025-06-02 16:21:52 +0900

    fix: seriarize value

--- src/feature.rs
+++ src/feature.rs
@@ -25,6 +25,28 @@ pub enum Value {
   Null,
 }
 
+#[cfg(feature = "serde")]
+use serde::{Serialize, Serializer};
+
+#[cfg(feature = "serde")]
+impl Serialize for Value {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    match self {
+      Value::String(v) => serializer.serialize_str(v),
+      Value::Float(v) => serializer.serialize_f32(*v),
+      Value::Double(v) => serializer.serialize_f64(*v),
+      Value::Int(v) => serializer.serialize_i64(*v),
+      Value::SInt(v) => serializer.serialize_i64(*v),
+      Value::UInt(v) => serializer.serialize_u64(*v),
+      Value::Bool(v) => serializer.serialize_bool(*v),
+      Value::Null => serializer.serialize_none(),
+    }
+  }
+}
+
 /// A structure representing a feature in a vector tile.
 #[derive(Debug, Clone)]
 pub struct Feature {
